55f94f968a6d653bb6cc707174a2dfbff81d4360
github username <github email address>
2020-05-04 02:57:22 -0400

    Added display functionality.

--- lib/star_wars_cli/cli.rb
+++ lib/star_wars_cli/cli.rb
@@ -5,8 +5,10 @@ class Cli
         puts " "
         puts Api.get_all_info
         print_people(Person.all)
-        @number = gets.strip.downcase 
+        input = gets.strip 
+        display_selection(input)
     end
+    
     def print_people(people)
         puts " "
         puts "Please select a person that you would like to learn more about"
@@ -15,6 +17,20 @@ class Cli
         end
         puts " "
     end
+
+    def display_selection(input)
+        person = Person.all[input.to_i - 1]
+        puts "Name: #{person.name}"
+        puts "Gender: #{person.gender}"
+        puts "Birth Year: #{person.birth_year}"
+        puts "Height: #{person.height}"
+        puts "Mass: #{person.mass}"
+        puts "Hair Color: #{person.hair_color}"
+        puts "Eye Color: #{person.eye_color}"
+        puts "Skin Color: #{person.skin_color}"
+    end
+
+    
 
 end
 
